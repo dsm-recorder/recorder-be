630a0557c1353505fc5452f5a4e394edfe57d856
gilgeunwoo <kesksjxhx@gmail.com>
2023-10-18 12:25:22 +0900

    setting: (#9) module setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,17 @@ import { UserModule } from './infrastructure/global/module/user.module';
 import { TypeormConfigModule } from './infrastructure/global/config/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './infrastructure/global/module/auth.module';
+import { ProjectModule } from './infrastructure/global/module/project.module';
+import { GithubModule } from './infrastructure/global/module/github.module';
 
 @Module({
   imports: [
     UserModule,
     AuthModule,
+    ProjectModule,
+    GithubModule,
     TypeormConfigModule,
-    ConfigModule.forRoot({ isGlobal: true })]
+    ConfigModule.forRoot({ isGlobal: true })
+  ]
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/infrastructure/global/module/project.module.ts
+++ src/infrastructure/global/module/project.module.ts
@@ -5,12 +5,17 @@ import { ProjectPersistenceAdapter } from '../../project/presistence/project.per
 import { ProjectPort } from '../../../application/project/spi/project.spi';
 import { ProjectMapper } from '../../project/presistence/project.mapper';
 import { UserTypeormEntity } from '../../user/persistence/user.entity';
+import { ProjectWebAdapter } from '../../project/presentation/project.web.adapter';
+import { QueryCurrentRepositoryUseCase } from '../../../application/project/usecase/query-current-repository.usecase';
+import { GithubModule } from './github.module';
+import { CreateProjectUseCase } from '../../../application/project/usecase/create-project.usecase';
 
 const PROJECT_PORT = { provide: ProjectPort, useClass: ProjectPersistenceAdapter };
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProjectTypeormEntity, UserTypeormEntity])],
-  providers: [PROJECT_PORT, ProjectMapper],
-  exports: [PROJECT_PORT]
+  imports: [TypeOrmModule.forFeature([ProjectTypeormEntity, UserTypeormEntity]), GithubModule],
+  providers: [PROJECT_PORT, ProjectMapper, QueryCurrentRepositoryUseCase, CreateProjectUseCase],
+  exports: [PROJECT_PORT],
+  controllers: [ProjectWebAdapter]
 })
 export class ProjectModule {}

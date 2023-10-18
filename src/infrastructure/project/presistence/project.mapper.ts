5a49b3d129bd2189c8dd90250e2ef292a3703a9c
gilgeunwoo <kesksjxhx@gmail.com>
2023-10-18 12:28:58 +0900

    feat: project mapper

---
+++ src/infrastructure/project/presistence/project.mapper.ts
@@ -0,0 +1,39 @@
+import { Project } from '../../../application/project/project';
+import { ProjectTypeormEntity } from './project.entity';
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { UserTypeormEntity } from '../../user/persistence/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class ProjectMapper {
+  constructor(
+    @InjectRepository(UserTypeormEntity)
+    private readonly userRepository: Repository<UserTypeormEntity>
+  ) {}
+
+  async toDomain(entity: ProjectTypeormEntity): Promise<Project> {
+    return {
+      id: entity.id,
+      userId: entity.user.id,
+      skills: entity.skills,
+      name: entity.name,
+      logoUrl: entity.logoUrl,
+      isPublic: entity.isPublic,
+      description: entity.description
+    };
+  }
+
+  async toEntity(domain: Project): Promise<ProjectTypeormEntity> {
+    let user = await this.userRepository.findOneBy({ id: domain.userId });
+    return {
+      id: domain.id,
+      user: user,
+      skills: domain.skills,
+      name: domain.name,
+      logoUrl: domain.logoUrl,
+      isPublic: domain.isPublic,
+      description: domain.description
+    };
+  }
+}

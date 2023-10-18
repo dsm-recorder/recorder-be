a000801bf565b30186ec9b6f8e4b6a9a8c40dce3
gilgeunwoo <kesksjxhx@gmail.com>
2023-10-18 12:30:03 +0900

    feat: dto

---
+++ src/application/project/dto/project.dto.ts
@@ -0,0 +1,5 @@
+export class QueryCurrentRepositoryResponse {
+  repos: {
+    name: string;
+  }[];
+}
---
+++ src/infrastructure/project/presentation/dto/project.web.dto.ts
@@ -0,0 +1,4 @@
+export class CreateProjectRequest {
+  repositoryName: string;
+  skills: string[];
+}

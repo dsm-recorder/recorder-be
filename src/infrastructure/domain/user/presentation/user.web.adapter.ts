b3a5a44b9b92e49c8d98a9d571bb9d381f0fe82c
이하성 <tedsoftj1123@gmail.com>
2023-10-28 15:56:50 +0900

    feat: (#18) update profile usecase

--- src/infrastructure/domain/user/presentation/user.web.adapter.ts
+++ src/infrastructure/domain/user/presentation/user.web.adapter.ts
@@ -1,13 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, HttpCode, Patch } from '@nestjs/common';
 import { Permission } from '../../../global/decorator/authority.decorator';
 import { Authority } from '../persistence/user.entity';
 import { CurrentUser } from '../../../global/decorator/current-user.decorator';
 import { User } from '../../../../application/domain/user/user';
-import { QueryMyInfoResponse } from './dto/user.web.dto';
+import { UpdateProfileUseCase } from '../../../../application/domain/user/usecase/update-profile.usecase';
+import { UpdateProfileRequest } from './dto/user.web.dto';
 
 @Controller('users')
 export class UserWebAdapter {
-    constructor() {}
+
+    constructor(
+        private readonly updateProfileUseCase: UpdateProfileUseCase
+    ) {}
+
+    @HttpCode(204)
+    @Permission([Authority.USER])
+    @Patch('/profile')
+    async updateProfile(@CurrentUser() user: User, @Body() request: UpdateProfileRequest) {
+        await this.updateProfileUseCase.execute(user, request.profileImageUrl);
+    }
 
     @Permission([Authority.USER])
     @Get('/my')

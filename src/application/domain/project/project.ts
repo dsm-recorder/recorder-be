9f378332a7d83296ed3727036969ac8a2aeca5fd
gilgeunwoo <kesksjxhx@gmail.com>
2023-11-02 17:36:05 +0900

    fix: (#27) conflict

--- src/application/domain/project/project.ts
+++ src/application/domain/project/project.ts
@@ -1,4 +1,26 @@
 export class Project {
+    constructor(
+        id: string,
+        userId: string,
+        name: string,
+        skills: string[],
+        isPublic: boolean,
+        logoUrl: string,
+        githubOwnerRepository: string,
+        description: string,
+        createdAt: Date,
+    ) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+        this.skills = skills;
+        this.isPublic = isPublic;
+        this.logoUrl = logoUrl;
+        this.githubOwnerRepository = githubOwnerRepository;
+        this.description = description;
+        this.createdAt = createdAt;
+    }
+
     id?: string;
     userId: string;
     name: string;

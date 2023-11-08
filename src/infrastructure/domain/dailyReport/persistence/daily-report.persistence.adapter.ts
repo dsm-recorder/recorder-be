1f0cf86d614bcd6a5f0bb33dee2bd3a27b2bfb8b
이하성 <tedsoftj1123@gmail.com>
2023-11-08 16:17:46 +0900

    setting: (#29) daily report persistence

---
+++ src/infrastructure/domain/dailyReport/persistence/daily-report.persistence.adapter.ts
@@ -0,0 +1,82 @@
+import { DailyReportPort } from '../../../../application/domain/dailyReport/spi/daily-report.spi';
+import { Injectable } from '@nestjs/common';
+import { convert, LocalDate } from 'js-joda';
+import { DailyReport } from '../../../../application/domain/dailyReport/daily-report';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DailyReportTypeormEntity } from './daily-report.entity';
+import { Equal, Repository } from 'typeorm';
+import { DailyReportMapper } from './daily-report.mapper';
+
+@Injectable()
+export class DailyReportPersistenceAdapter implements DailyReportPort {
+    constructor(
+        @InjectRepository(DailyReportTypeormEntity)
+        private readonly dailyReportRepository: Repository<DailyReportTypeormEntity>,
+        private readonly dilayReportMapper: DailyReportMapper,
+    ) {}
+
+    async queryDailyReportsByDateAndProjectId(date: LocalDate, projectId: string): Promise<DailyReport[]> {
+        const dailyReports = await this.dailyReportRepository.find({
+            where: {
+                date: Equal(convert(date).toDate()),
+                project: {
+                    id: projectId,
+                },
+            },
+            relations: {
+                project: true,
+            },
+        });
+
+        return Promise.all(
+            dailyReports.map(async (dailyReport) => {
+                return await this.dilayReportMapper.toDomain(dailyReport);
+            }),
+        );
+    }
+
+    async saveDailyReport(dailyReport: DailyReport): Promise<void> {
+        await this.dailyReportRepository.save(
+            await this.dilayReportMapper.toEntity(dailyReport),
+        );
+    }
+
+    async queryDailyReportById(dailyReportId: string): Promise<DailyReport> {
+        return await this.dilayReportMapper.toDomain(
+            await this.dailyReportRepository.findOne({
+                where: {
+                    id: dailyReportId,
+                },
+                relations: {
+                    project: true,
+                },
+            }),
+        );
+    }
+
+    async deleteDailyReport(dailyReportId: string): Promise<void> {
+        await this.dailyReportRepository.createQueryBuilder('dailyReport')
+            .delete()
+            .where('id = :id', { id: dailyReportId })
+            .execute();
+    }
+
+    async queryDailyReportsByProjectId(projectId: string): Promise<DailyReport[]> {
+        const dailyReports = await this.dailyReportRepository.find({
+            where: {
+                project: { id: projectId },
+            },
+            relations: {
+                project: true,
+            },
+            order: { date: 'desc' },
+        });
+
+        return Promise.all(
+            dailyReports.map(async (dailyReport) => {
+                return await this.dilayReportMapper.toDomain(dailyReport);
+            }),
+        );
+    }
+
+}

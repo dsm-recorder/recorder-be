85872b0a9b9f4cad62bbbb6eedf2c8fc50c1dae1
이하성 <tedsoftj1123@gmail.com>
2023-11-08 16:18:17 +0900

    feat: (#29) daily report usecases

---
+++ src/application/domain/dailyReport/usecase/create-todo.usecase.ts
@@ -0,0 +1,32 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { DailyReportPort } from '../spi/daily-report.spi';
+import { CreateTodoRequest } from '../../../../infrastructure/domain/dailyReport/presentation/dto/daily-report.web.dto';
+import { DailyReport } from '../daily-report';
+import { LocalDate } from 'js-joda';
+import { ProjectPort } from '../../project/spi/project.spi';
+
+@Injectable()
+export class CreateTodoUseCase {
+
+    constructor(
+        @Inject(DailyReportPort)
+        private readonly dailyReportPort: DailyReportPort,
+        @Inject(ProjectPort)
+        private readonly projectPort: ProjectPort,
+    ) {}
+
+    async execute(request: CreateTodoRequest, projectId: string): Promise<void> {
+        const project = await this.projectPort.queryProjectById(projectId);
+        if (!project) {
+            throw new NotFoundException('Project Not Found');
+        }
+        await this.dailyReportPort.saveDailyReport(
+            new DailyReport(
+                request.content,
+                false,
+                LocalDate.now(),
+                projectId,
+            ),
+        );
+    }
+}
---
+++ src/application/domain/dailyReport/usecase/delete-todo.usecase.ts
@@ -0,0 +1,19 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { DailyReportPort } from '../spi/daily-report.spi';
+
+@Injectable()
+export class DeleteTodoUseCase {
+    constructor(
+        @Inject(DailyReportPort)
+        private readonly dailyReportPort: DailyReportPort
+    ) {}
+
+    async execute(dailyReportId: string): Promise<void> {
+        const dailyReport = await this.dailyReportPort.queryDailyReportById(dailyReportId);
+        if (!dailyReport) {
+            throw new NotFoundException("Daily Report Not Found");
+        }
+
+        await this.dailyReportPort.deleteDailyReport(dailyReport.id);
+    }
+}

52d0e59c6272ee3d6051d4376b75b83292ce48d3
gilgeunwoo <kesksjxhx@gmail.com>
2023-11-09 09:51:01 +0900

    refactor: (#main) 500 console log

--- src/infrastructure/thirdparty/axios/util/axios.util.ts
+++ src/infrastructure/thirdparty/axios/util/axios.util.ts
@@ -4,13 +4,14 @@ import { InternalServerErrorException, NotFoundException } from '@nestjs/common'
 export async function getAndHandleError<T = any>(url: string): Promise<T> {
     return await axios
         .get<T>(url)
-        .then(response => {
+        .then((response) => {
             return response.data;
         })
-        .catch(error => {
+        .catch((error) => {
             if (error.request.res.statusCode === 404) {
                 throw new NotFoundException(error.request.statusMessage);
             } else {
+                console.log(error);
                 throw new InternalServerErrorException(error.request.statusMessage);
             }
         });

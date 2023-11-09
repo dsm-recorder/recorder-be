496c08231a8a47f259b20dfde4006aff704ce424
이하성 <tedsoftj1123@gmail.com>
2023-11-09 17:04:08 +0900

    feat: (#32) update, query pr record url mapping

--- src/infrastructure/domain/pr_record/presentation/pr-record.web.adapter.ts
+++ src/infrastructure/domain/pr_record/presentation/pr-record.web.adapter.ts
@@ -1,14 +1,23 @@
-import { Body, Controller, HttpCode, Param, Post, Query } from '@nestjs/common';
-import { CreatePRRecordUseCase } from '../../../../application/domain/pr_record/usecase/create-pr-record.usecase';
+import { Body, Controller, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import {
+    QueryProjectPrRecordsUseCase
+} from '../../../../application/domain/pr_record/usecase/query-project-pr-records.usecase';
 import { Permission } from '../../../global/decorator/authority.decorator';
 import { Authority } from '../../user/persistence/user.entity';
-import { CreatePRRecordRequest } from '../../../../application/domain/pr_record/dto/pr-record.dto';
+import { QueryProjectPrRecordsResponse } from '../../../../application/domain/pr_record/dto/pr-record.dto';
 import { CurrentUser } from '../../../global/decorator/current-user.decorator';
 import { User } from '../../../../application/domain/user/user';
+import { CreatePRRecordUseCase } from '../../../../application/domain/pr_record/usecase/create-pr-record.usecase';
+import { UpdatePrRecordUseCase } from '../../../../application/domain/pr_record/usecase/update-pr-record.usecase';
+import { CreatePRRecordRequest, UpdatePrRecordRequest } from './pr-record.web.dto';
 
 @Controller('pr-records')
-export class PRRecordWebAdapter {
-    constructor(private readonly createPRRecordUseCase: CreatePRRecordUseCase) {}
+export class PrRecordWebAdapter {
+    constructor(
+        private readonly queryProjectPrRecordsUseCase: QueryProjectPrRecordsUseCase,
+        private readonly createPRRecordUseCase: CreatePRRecordUseCase,
+        private readonly updatePrRecordUseCase: UpdatePrRecordUseCase
+    ) {}
 
     @Permission([Authority.USER])
     @HttpCode(201)
@@ -16,8 +25,21 @@ export class PRRecordWebAdapter {
     async createPRRecord(
         @Param('projectId') projectId: string,
         @Body() request: CreatePRRecordRequest,
-        @CurrentUser() user: User,
-    ) {
+        @CurrentUser() user: User
+    ): Promise<void> {
         await this.createPRRecordUseCase.execute(projectId, request, user);
+    }
+
+    @Permission([Authority.USER])
+    @Get(':projectId')
+    async queryProjectPrRecords(@Param('projectId') projectId: string): Promise<QueryProjectPrRecordsResponse> {
+        return await this.queryProjectPrRecordsUseCase.execute(projectId);
+    }
+
+    @HttpCode(204)
+    @Permission([Authority.USER])
+    @Patch(':prRecordId')
+    async updatePrRecord(@Param('prRecordId') prRecordId: string, @Body() request: UpdatePrRecordRequest): Promise<void> {
+        await this.updatePrRecordUseCase.execute(request, prRecordId);
     }
 }
---
+++ src/infrastructure/domain/pr_record/presentation/pr-record.web.dto.ts
@@ -0,0 +1,17 @@
+import { RecordType } from '../../../../application/domain/pr_record/pr-record';
+
+export class UpdatePrRecordRequest {
+    title: string;
+    content: string;
+    importance: number;
+    solution?: string;
+    type: RecordType
+}
+
+export class CreatePRRecordRequest {
+    title: string;
+    content: string;
+    importance: number;
+    solution?: string;
+    type: RecordType;
+}

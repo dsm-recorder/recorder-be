585b046c073d66641260dd6d2e9fa77adeb4d120
gilgeunwoo <kesksjxhx@gmail.com>
2023-11-14 09:46:19 +0900

    feat: (#39) 예외처리

--- src/application/domain/like/usecase/like.usecase.ts
+++ src/application/domain/like/usecase/like.usecase.ts
@@ -1,16 +1,27 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { LikePort } from '../spi/like.spi';
 import { User } from '../../user/user';
 import { Like } from '../like';
+import { ProjectPort } from '../../project/spi/project.spi';
 
 @Injectable()
 export class LikeUseCase {
     constructor(
         @Inject(LikePort)
         private readonly likePort: LikePort,
+        @Inject(ProjectPort)
+        private readonly projectPort: ProjectPort,
     ) {}
 
     async execute(projectId: string, user: User) {
+        const project = await this.projectPort.queryProjectById(projectId);
+        if (!project) {
+            throw new NotFoundException('Project Not Found');
+        }
+        if (!project.isPublished) {
+            throw new ForbiddenException('Project is Not Published');
+        }
+
         const like: Like = await this.likePort.queryLikeByUserIdAndProjectId(user.id, projectId);
         if (!like) {
             await this.likePort.saveLike(new Like(projectId, user.id));

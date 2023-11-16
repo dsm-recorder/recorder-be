df25d10c1eb54a0a6f897caa28e41c4462615017
gilgeunwoo <kesksjxhx@gmail.com>
2023-11-16 19:48:43 +0900

    feat: (#46) 첨부파일 수정 추가

--- src/application/domain/pr_record/usecase/update-pr-record.usecase.ts
+++ src/application/domain/pr_record/usecase/update-pr-record.usecase.ts
@@ -1,6 +1,7 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { PrRecordPort } from '../spi/pr-record.spi';
 import { UpdatePrRecordRequest } from '../../../../infrastructure/domain/pr_record/presentation/pr-record.web.dto';
+import { RecordAttachment } from '../record-attachment';
 
 @Injectable()
 export class UpdatePrRecordUseCase {
@@ -11,6 +12,11 @@ export class UpdatePrRecordUseCase {
 
     async execute(request: UpdatePrRecordRequest, prRecordId: string): Promise<void> {
         const prRecord = await this.prRecordPort.queryPrRecordById(prRecordId);
+        if (!prRecord) {
+            throw new NotFoundException('PrRecord Not Found');
+        }
+        const attachments = await this.prRecordPort.queryAttachmentsByPrRecordId(prRecordId);
+
         prRecord.update(
             request.title,
             request.content,
@@ -18,7 +24,12 @@ export class UpdatePrRecordUseCase {
             request.type,
             request.solution
         );
-
+        await this.prRecordPort.deleteAllAttachments(attachments);
+        await this.prRecordPort.saveAllAttachments(
+            request.attachmentUrls.map(
+                (attachmentUrl) => new RecordAttachment(attachmentUrl, prRecord.id)
+            )
+        );
         await this.prRecordPort.savePrRecord(prRecord);
     }
 }

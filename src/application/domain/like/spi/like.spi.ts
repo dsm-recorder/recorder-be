a07fe6d8c9f45f1670592b437870c91d582e603f
이하성 <tedsoftj1123@gmail.com>
2023-11-16 20:24:03 +0900

    feat: (#51) query published project detail usecase

--- src/application/domain/like/spi/like.spi.ts
+++ src/application/domain/like/spi/like.spi.ts
@@ -2,8 +2,12 @@ import { Like } from '../like';
 
 export interface LikePort {
     saveLike(like: Like): Promise<Like>;
+
     deleteLike(like: Like): Promise<void>;
+
     queryLikeByUserIdAndProjectId(userId: string, projectId: string): Promise<Like>;
+
+    existsLikeByProjectIdAndUserId(projectId: string, userId: string): Promise<boolean>;
 }
 
 export const LikePort = Symbol('ILikePort');

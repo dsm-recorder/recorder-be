d54eb33ad46dd72a42fc72a02c3f66e676e174ad
이하성 <tedsoftj1123@gmail.com>
2023-11-18 23:48:58 +0900

    feat: (#55) add project not found case

--- src/application/domain/comment/usecase/query-project-comments.usecase.ts
+++ src/application/domain/comment/usecase/query-project-comments.usecase.ts
@@ -1,15 +1,22 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CommentPort } from '../spi/comment.spi';
 import { QueryProjectCommentsResponse } from '../dto/comment.dto';
+import { ProjectPort } from '../../project/spi/project.spi';
 
 @Injectable()
 export class QueryProjectCommentsUseCase {
     constructor(
         @Inject(CommentPort)
-        private readonly commentPort: CommentPort
+        private readonly commentPort: CommentPort,
+
+        @Inject(ProjectPort)
+        private readonly projectPort: ProjectPort
     ) {}
 
     async execute(projectId: string, userId: string): Promise<QueryProjectCommentsResponse> {
+        if (!await this.projectPort.existsProjectById(projectId)) {
+            throw new NotFoundException('Project Not Found');
+        }
         const comments = await this.commentPort.queryProjectComments(projectId, userId);
 
         return {

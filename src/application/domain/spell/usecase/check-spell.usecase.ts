9e7c7a9bee688e5f636be7491541ac3f018a55e4
gilgeunwoo <kesksjxhx@gmail.com>
2023-11-18 21:58:53 +0900

    fix: (#main) 맞춤법 검사 500

--- src/application/domain/spell/usecase/check-spell.usecase.ts
+++ src/application/domain/spell/usecase/check-spell.usecase.ts
@@ -14,7 +14,7 @@ export class CheckSpellUseCase {
         const data = html.slice(html.indexOf('data = [') + 8, html.lastIndexOf('];'));
         let json;
         try {
-            const json = JSON.parse(data);
+            json = JSON.parse(data);
         } catch (e) {
             throw new BadRequestException(
                 '맞춤법과 문법 오류를 찾지 못했습니다. 기술적 한계로 찾지 못한 맞춤법 오류나 문법 오류가 있을 수 있습니다.'
